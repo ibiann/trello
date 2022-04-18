ccbe85c0af071b8ff086557fb924d95a3771d3c0
Trung Phan <61691905+ibiann@users.noreply.github.com>
2022-04-18 13:30:36 +0700

    continue

--- trello-app/src/App.js
+++ trello-app/src/App.js
@@ -2,8 +2,57 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="hello-world">Hello World</h1>
+    <div className="trello-container">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-column">
+        <div className="column">
+        <header className="trello-header">
+              Brainstorm
+        </header>
+          <ul>
+            <li>
+            <img src="https://picsum.photos/200/200" alt='trung-img'/>
+            </li>
+            <li>
+            Green vines attached to the trunk of the tree
+            </li>
+            <li>
+            Green vines attached to the trunk of the tree
+            </li>
+            <li>
+            Green vines attached to the trunk of the tree
+            </li>
+            <li>
+            Green vines attached to the trunk of the tree
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+          </div>
+          <div className="column">
+        <header className="trello-header">
+              Brainstorm
+        </header>
+          <ul>
+            <li>
+            <img src="https://picsum.photos/200/200" alt='trung-img'/>
+            </li>
+            <li>
+            Green vines attached to the trunk of the tree
+            </li>
+            <li>
+            Green vines attached to the trunk of the tree
+            </li>
+            <li>
+            Green vines attached to the trunk of the tree
+            </li>
+            <li>
+            Green vines attached to the trunk of the tree
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+          </div>
+      </div>
     </div>
   );
 }
